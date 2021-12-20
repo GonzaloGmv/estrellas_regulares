3631e73b91d5f11c66d76740f350e59b92b5845c
GonzaloGmv <91721237+GonzaloGmv@users.noreply.github.com>
2021-12-20 19:57:04 +0100

    ya funciiona pero tarda demasiado

--- estrella.py
+++ estrella.py
@@ -1,8 +1,19 @@
+import turtle
 from turtle import *
+perimetro = 0
+acabado = False
+i = 1
+n = int(input())
 begin_fill()
 while True:
     forward(200)
-    left(180-45)
+    left(180-i)
+    perimetro = perimetro + 200
     if abs(pos()) < 1:
-        break
+        if perimetro == 200 * n:
+            break
+        else:
+            i = i + 1
+            turtle.reset()
+            perimetro = 0
 done()
